1bda2a843d8bcc368f6a94b4142dd7115352f26f
TomoMayumi <TomoMayumi@gmail.com>
2020-03-03 09:20:11 +0900

    テストが通るように修正（userとmicropostを関連づけたことに対応）

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 
 class MicropostsControllerTest < ActionDispatch::IntegrationTest
   setup do
+    @user = users(:one)
+    #post users_url, params: { user: { name: @user.name, email: @user.email } }
     @micropost = microposts(:one)
   end
 
@@ -17,7 +19,7 @@ class MicropostsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create micropost" do
     assert_difference('Micropost.count') do
-      post microposts_url, params: { micropost: { content: @micropost.content, user_id: @micropost.user_id } }
+      post microposts_url, params: { micropost: { content: @micropost.content, user_id: @user.id } }
     end
 
     assert_redirected_to micropost_url(Micropost.last)
@@ -34,7 +36,7 @@ class MicropostsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update micropost" do
-    patch micropost_url(@micropost), params: { micropost: { content: @micropost.content, user_id: @micropost.user_id } }
+    patch micropost_url(@micropost), params: { micropost: { content: @micropost.content, user_id: @user.id } }
     assert_redirected_to micropost_url(@micropost)
   end
 
