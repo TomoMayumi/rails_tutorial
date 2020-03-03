776be50bf06c67495079e2864206787a8b2f08a7
TomoMayumi <TomoMayumi@gmail.com>
2020-03-03 10:01:34 +0900

    ルートの修正と不要なコード削除

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   resources :microposts
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'application#goodbye'
+  root 'static_pages#home'
 end
