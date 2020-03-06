26b591b9cb12debab9871b68fe8f089d8f15466d
TomoMayumi <TomoMayumi@gmail.com>
2020-03-06 10:22:06 +0900

    userモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_02_004131) do
-
-  create_table "microposts", force: :cascade do |t|
-    t.text "content"
-    t.integer "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2020_03_06_011647) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
