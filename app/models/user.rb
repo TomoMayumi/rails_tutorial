b919bbadf9acce0ad85873ddacf1b1a07853d316
TomoMayumi <TomoMayumi@gmail.com>
2020-03-12 12:59:41 +0900

    emailはDBによらず大文字小文字を区別しない仕様とするため、全て小文字にしてから格納する

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
+  before_save {
+    email.downcase!
+  }
   validates :name,
             {
               presence: true,
